900e730e913b05bee57ed9e5b299e8854cdf81ed
tengdann <tengdann@umich.edu>
2019-03-19 21:53:17 -0400

    interim

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -12,12 +12,13 @@ from sklearn.ensemble import RandomForestClassifier
 # from muffnn import MLPClassifier, MLPRegressor
 
 # Create dataframe for model output
-curdir = r'C:\Users\dteng\Desktop\lhs712\assignment_2\dataset\unlabeled-test-data\Gastroenterology'
-# curdir = r'C:\Users\mrasianman3\Desktop\lhs712\assignment_2\dataset\unlabeled-test-data\Gastroenterology'
+# curdir = r'C:\Users\dteng\Desktop\lhs712\assignment_2\dataset\unlabeled-test-data\Gastroenterology'
+curdir = r'C:\Users\mrasianman3\Desktop\lhs712\assignment_2\dataset\unlabeled-test-data\Gastroenterology'
 files = list()
 for file in os.listdir(curdir):
     files.append(file)
 
+files.sort()
 df = pd.DataFrame(data = files, columns = ['Filename'])
 
 # Preprocessing steps
@@ -42,7 +43,7 @@ if cont.lower() == 'y':
         [
             ('vect', CountVectorizer(decode_error = 'ignore', strip_accents = 'unicode', lowercase = True)),
             ('tfidf', TfidfTransformer(use_idf = False)),
-            ('clf', naive_bayes.MultinomialNB())
+            ('clf', naive_bayes.MultinomialNB(fit_prior = False))
         ]
     )
     clfrNB = clfrNB.fit(train_data.data, train_data.target)
@@ -59,7 +60,7 @@ if cont.lower() == 'y':
         [
             ('vect', CountVectorizer(decode_error = 'ignore', strip_accents = 'unicode', lowercase = True)),
             ('tfidf', TfidfTransformer(use_idf = True)),
-            ('clf', LogisticRegression(class_weight = 'balanced', verbose = 1))
+            ('clf', LogisticRegression(class_weight = 'balanced', multi_class = 'auto', verbose = 1))
         ]
     )
     clfrLR = clfrLR.fit(train_data.data, train_data.target)
@@ -75,7 +76,7 @@ if cont.lower() == 'y':
     clfrSVM = Pipeline(
         [
             ('vect', CountVectorizer(decode_error = 'ignore', strip_accents = 'unicode')),
-            ('tfidf', TfidfTransformer(use_idf = False)),
+            ('tfidf', TfidfTransformer(use_idf = True)),
             ('clf', svm.LinearSVC())
         ]
     )
@@ -144,8 +145,7 @@ if cont.lower() == 'y':
         'vect__ngram_range': [(1, 1), (1, 2), (1, 3)],
         'tfidf__use_idf': (True, False),
         'clf__solver': ('newton-cg', 'sag', 'saga', 'lbfgs'),
-        'clf__multi_class': ('ovr', 'multinomial'),
-        'clf__C': (1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1)
+        'clf__C': (1e-6, 1e-4, 1e-2, 1)
     }
 
     gs_clflr = GridSearchCV(clfrLR, params_gslr, cv = 10, n_jobs = -1)
